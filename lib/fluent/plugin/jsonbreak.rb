24819efcffbc5f382f3d7618faa9d0ec05a0562b
Uchio KONDO <udzura@udzura.jp>
2015-10-28 19:26:24 +0900

    Initial

---
+++ lib/fluent/plugin/jsonbreak.rb
@@ -0,0 +1,9 @@
+require "fluent/plugin/jsonbreak/version"
+
+module Fluent
+  module Plugin
+    module Jsonbreak
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fluent/plugin/jsonbreak/version.rb
@@ -0,0 +1,7 @@
+module Fluent
+  module Plugin
+    module Jsonbreak
+      VERSION = "0.1.0"
+    end
+  end
+end
