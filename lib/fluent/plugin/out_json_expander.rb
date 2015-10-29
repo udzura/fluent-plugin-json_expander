c2511709d6d72744cae84e7e1dec16112a8b3b2c
Uchio KONDO <udzura@pepabo.com>
2015-10-29 19:37:43 +0900

    Introduce caching

--- lib/fluent/plugin/out_json_expander.rb
+++ lib/fluent/plugin/out_json_expander.rb
@@ -28,6 +28,8 @@ class Fluent::JsonExpanderOutput < Fluent::MultiOutput
 
     @outputs = []
     @mutex = Mutex.new
+    @mappings = {}
+    @invalid_mapping_keys = []
 
     templates = conf.elements.select{|e| e.name == 'template' }
     if templates.size != 1
@@ -35,6 +37,7 @@ class Fluent::JsonExpanderOutput < Fluent::MultiOutput
     end
 
     @template = templates.first
+    @expand_target_keys = scan_keys(@template)
   end
 
   def emit(tag, es, chain)
@@ -57,24 +60,60 @@ class Fluent::JsonExpanderOutput < Fluent::MultiOutput
     chain.next
   end
 
+  def shutdown
+    super
+    @mappings.values.each do |output|
+      output.shutdown
+    end
+    @mappings.clear
+  end
+
   private
+
+  SCAN_DATA_RE = /\$\{data\[([_a-zA-Z][_a-zA-Z0-9]*)\]\}/
+  def scan_keys(elm)
+    elm.inject([]) { |dst, (attr, value)|
+      dst.concat(value.scan(SCAN_DATA_RE).flatten)
+    }.sort.uniq
+  end
+
+  def to_mapping_key(data)
+    data.select{|k, _| @expand_target_keys.include? k }
+      .to_a
+      .sort_by(&:first)
+      .flatten
+      .join("::")
+  end
 
   def new_output(data)
     o = nil
     t = @template
+    map_key = to_mapping_key(data)
+    if @invalid_mapping_keys.include?(map_key)
+      return o, data
+    end
+
     begin
-      @mutex.synchronize do
-        e, data = expand_elm(t, data)
-        if e
-          o = Fluent::Plugin.new_output(@subtype)
-          o.configure(e)
-          o.start
-
-          @outputs.push(o)
+      o = @mappings[map_key]
+      if o
+        if @delete_used_key
+          @expand_target_keys.each{|k| data.delete(k) }
         end
-      end
+      else
+        @mutex.synchronize do
+          e, data = expand_elm(t, data)
+          if e
+            o = Fluent::Plugin.new_output(@subtype)
+            o.configure(e)
+            o.start
 
-      log.info "[out_json_expand] Expanded new output: #{@subtype}"
+            @outputs.push(o)
+            @mappings[map_key] = o
+          end
+        end
+
+        log.info "[out_json_expand] Expanded new output: #{@subtype}"
+      end
     rescue Fluent::ConfigError => e
       log.error "failed to configure sub output #{@subtype}: #{e.message}"
       log.error e.backtrace.join("\n")
@@ -87,16 +126,20 @@ class Fluent::JsonExpanderOutput < Fluent::MultiOutput
       o = nil
     end
 
+    unless o
+      @invalid_mapping_keys << map_key
+    end
+
     return o, data
   end
 
-  SCAN_DATA_RE = /\$\{data\[(?:[_a-zA-Z][_a-zA-Z0-9]*)\]\}/
+  GSUB_DATA_RE = /\$\{data\[(?:[_a-zA-Z][_a-zA-Z0-9]*)\]\}/
   SCAN_KEY_NAME_RE = /\[([_a-zA-Z][_a-zA-Z0-9]*)\]/
 
   def expand_elm(template, data)
     attr = {}
     template.each do |k, v|
-      v = v.gsub(SCAN_DATA_RE) do |matched|
+      v = v.gsub(GSUB_DATA_RE) do |matched|
         key_matched = matched.scan(SCAN_KEY_NAME_RE)[0]
         if !key_matched or !key_matched[0]
           raise(Fluent::ConfigError, "[BUG] data matched in template, but could not find key name")
